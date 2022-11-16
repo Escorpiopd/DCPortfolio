5081823c8af8c50b1dc885b6c3676f19ecb5bb0b
Escorpiopd <30816049+Escorpiopd@users.noreply.github.com>
2022-11-16 18:00:52 +0100

    Add files via upload

--- JavaScript/open.js
+++ JavaScript/open.js
@@ -1,27 +1,27 @@
 function abrirNuevoTab(i) {
     switch(i){
         case '1':
-            window.location.href = '../Htmls/ImgSound.html';
+            window.location.href = 'Htmls/ImgSound.html';
             break;
 
         case '2':
-            window.location.href = '../Htmls/Html.html';
+            window.location.href = 'Htmls/Html.html';
             break;
             
         case '3':
-            window.location.href = '../Htmls/Sql.html';
+            window.location.href = 'Htmls/Sql.html';
             break;
 
         case '4':
-            window.location.href = '../Htmls/Css.html';
+            window.location.href = 'Htmls/Css.html';
             break;
 
         case '5':
-            window.location.href = '../Htmls/Java.html';
+            window.location.href = 'Htmls/Java.html';
             break;
 
         case '6':
-            window.location.href = '../Htmls/Javascript.html';
+            window.location.href = 'Htmls/Javascript.html';
             break;
     }
     window.focus;
